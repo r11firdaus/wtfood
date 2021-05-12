465e5cbb649b5a3190bed8154014d7eb21915194
Reza Firdaus <rezafirdaus@Rezas-MacBook-Pro.local>
2021-05-12 15:23:55 +0700

    fix text display

--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -24,11 +24,10 @@ const Headline = props => {
             </div>
             <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
             </a>
             <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
+
             </a>
         </div>
     )
--- src/component/spinner/index.js
+++ src/component/spinner/index.js
@@ -4,7 +4,6 @@ const Spinner = () => {
     return (
         <div className="d-flex justify-content-center" style={{ marginTop: tinggi / 2 }}>
             <div className="spinner-grow text-primary" role="status">
-                <span className="sr-only">Loading...</span>
             </div>
         </div>
     )
